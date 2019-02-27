378cfa3bc008c619e12b2d587ed2160d85af0b76
Max Timkovich <mtimkovich@gmail.com>
2019-02-26 23:53:51 -0800

    animated background!

--- Background.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite
-
-var score = 0
-
-func _on_Timer_timeout():
-	# TODO: https://github.com/godotengine/godot/issues/16007
-	frame = (frame + 1) % (vframes * hframes)
--- Start.gd
+++ Start.gd
@@ -2,7 +2,7 @@ extends Label
 
 var opacity = 1
 
-func _gui_input(event):
+func _on_StartArea_gui_input(event):
 	if event is InputEventMouseButton and event.is_pressed():
 		$Coin.play()
 
@@ -10,5 +10,6 @@ func _on_Coin_finished():
 	get_tree().change_scene('res://Main.tscn')
 
 func _on_Timer_timeout():
+	# Blink
 	opacity = 1 - opacity
 	modulate.a = opacity
